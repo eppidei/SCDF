4a61856385a9ae774ad189a80900ee52a138a99f
marco <marco@6c43cc0a-5ffe-4778-b326-04effe5da469>
2014-06-07 21:32:25 +0000

    create iOs project workspace

---
+++ project/TestIOS/SCDF_Test/SCDF_Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SCDF_Test
+//
+//  Created by Marco Bertola on 07/06/14.
+//  Copyright (c) 2014 Marco Bertola. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
