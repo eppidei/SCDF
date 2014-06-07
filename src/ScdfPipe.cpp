4752cb525eb7f418b8a0cbefa42276e5b2c5a86a
andrea <andrea@6c43cc0a-5ffe-4778-b326-04effe5da469>
2014-06-07 10:35:43 +0000

    Pipes template methods

--- src/ScdfPipe.cpp
+++ src/ScdfPipe.cpp
@@ -52,35 +52,39 @@ s_bool ScdfPipe::Write(const s_char *buffer, s_int32 bytesToWrite, s_int32 *byte
     return bytesWritten1!=-1;
 }
 
-s_int32 ScdfPipe::SendMessage(PipeMessage msg)
+template s_int32 ScdfPipe::SendMessage<SensorData*> (SensorData *msg);
+
+template <class PipeMessage> s_int32 ScdfPipe::SendMessage(PipeMessage msg)
 {
     _ASSERT(!invalid);
     if (invalid) return 0;
     s_int32 bytesWritten=0;
-    s_int32 ret=Write((const s_char *)msg, SizeofPipeMessage, &bytesWritten);
+    s_int32 ret=Write((const s_char *)msg, sizeof(PipeMessage), &bytesWritten);
     if(!ret)
     {
         Logging::Instance()->log(_T("Error writing to pipe"));
         return 0;
     }
-    _ASSERT(bytesWritten==SizeofPipeMessage);
+    _ASSERT(bytesWritten==sizeof(PipeMessage));
     return 1;
 }
 
-PipeMessage ScdfPipe::ReadMessage()
+template SensorData* ScdfPipe::ReadMessage<SensorData*>();
+
+template <class PipeMessage> PipeMessage ScdfPipe::ReadMessage()
 {
     _ASSERT(!invalid);
     if (invalid) return NULL;
     s_int32 numBytesRead=0;
     PipeMessage msg;
-    if (Read((s_char *)msg, SizeofPipeMessage, &numBytesRead))
+    if (Read((s_char *)msg, sizeof(PipeMessage), &numBytesRead))
     {
         Logging::Instance()->log(_T("Error reading from pipe"));
         return NULL;
     }
     
     if(0==numBytesRead) return NULL;
-    _ASSERT(numBytesRead==SizeofPipeMessage);
+    _ASSERT(numBytesRead==sizeof(PipeMessage));
     return msg;
 }
 
--- src/ScdfPipe.h
+++ src/ScdfPipe.h
@@ -10,23 +10,21 @@
 
 namespace scdf
 {
-    class SensorData;
     typedef s_int32 PipeDescriptor;
-    typedef SensorData* PipeMessage;
 
     class ScdfPipe
     {
         PipeDescriptor pd[2];
-        const s_int32 SizeofPipeMessage=sizeof(PipeMessage);
         s_bool invalid;
     
         void Close();
         void Open();
         s_bool Read(s_char *buffer, s_int32 bytesToRead, s_int32 *bytesRead);
         s_bool Write(const s_char *buffer, s_int32 bytesToWrite, s_int32 *bytesWritten);
+        
     public:
-        s_int32 SendMessage(PipeMessage msg);
-        PipeMessage ReadMessage();
+        template <class PipeMessage> s_int32 SendMessage(PipeMessage msg);
+        template <class PipeMessage> PipeMessage ReadMessage();
         ScdfPipe();
         ~ScdfPipe();
     };
