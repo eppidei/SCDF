29a6897402992b7bcf96d88777d9f33eab54261a
athos <athos@6c43cc0a-5ffe-4778-b326-04effe5da469>
2014-06-18 23:24:51 +0000

    android: minimal working makefile - missing files

--- project/TestAndroid/jni/Android.mk
+++ project/TestAndroid/jni/Android.mk
@@ -1,8 +1,20 @@
-LOCAL_PATH := $(call my-dir)
+APP_LOCAL_PATH := $(call my-dir)
+SCDF_TRUNK := /home/athos/SCDF/trunk
+SCDF_INCLUDE := $(SCDF_TRUNK)/src/include
+SCDF_SRC := $(SCDF_TRUNK)/src
 
 include $(CLEAR_VARS)
-LOCAL_LDLIBS += -landroid -llog
-LOCAL_MODULE    := scdftest
-LOCAL_SRC_FILES := scdftest.cpp
+LOCAL_PATH := $(SCDF_SRC)
+LOCAL_MODULE := scdf
+LOCAL_C_INCLUDES := $(SCDF_INCLUDE)
+#LOCAL_LDLIBS += -landroid -llog
+LOCAL_SRC_FILES := Sensor.cpp
+include $(BUILD_STATIC_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE    := scdftest
+LOCAL_PATH:= $(APP_LOCAL_PATH)
+LOCAL_LDLIBS += -landroid -llog
+LOCAL_STATIC_LIBRARIES := scdf
+LOCAL_SRC_FILES := scdftest.cpp
 include $(BUILD_SHARED_LIBRARY)
