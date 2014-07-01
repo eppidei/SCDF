8e0434c4533dcbdabb993691584ec610e627403f
andrea <andrea@6c43cc0a-5ffe-4778-b326-04effe5da469>
2014-07-01 22:17:27 +0000

    Added PipesManager.h

---
+++ trunk/src/include/PipesManager.h
@@ -0,0 +1,35 @@
+//
+//  PipesManager.h
+//  SCDF
+//
+//  Created by Andrea Scuderi on 01/06/14.
+//
+//
+
+#include "Sensor.h"
+
+namespace scdf
+{
+    class CustomPipe;
+    class PipesManager;
+    PipesManager *thePipesManager();
+    class PipesManager
+    {
+        std::vector<CustomPipe*> pipes;
+        std::vector<CustomPipe*> returnPipes;
+        
+        void CreatePipes();
+    public:
+        void CreateReturnPipes();
+        std::vector<CustomPipe*>* GetReturnPipes();
+        void InitReturnPipes(SensorType type, s_int32 numSamples);
+        friend PipesManager *thePipesManager();
+        SensorData *ReadFromPipe(SensorType type);
+        SensorData *ReadFromReturnPipe(SensorType type);
+        s_int32 WriteOnReturnPipe(SensorType type, SensorData *data);
+        s_int32 WriteOnPipe(SensorType type, SensorData *data);
+        s_int32 NumPipes();
+        void InitPipes();
+    };
+
+}
