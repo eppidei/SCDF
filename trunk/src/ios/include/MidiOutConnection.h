49073204971cf6fd7283619da384d771016c5b43
marco <marco@6c43cc0a-5ffe-4778-b326-04effe5da469>
2014-12-16 01:18:01 +0000

    start adding MIDI out for iOs

--- trunk/src/ios/include/MidiOutConnection.h
+++ trunk/src/ios/include/MidiOutConnection.h
@@ -19,7 +19,7 @@ namespace Scdf  {
         class Listener
         {
             virtual void OnConnectionLost(MidiOutConnection* connection) = 0;
-        }
+        };
         
         static s_int32 GetNumAvailableOutputs();
         static std::string GetOutputName(s_int32 index);
@@ -27,11 +27,11 @@ namespace Scdf  {
         static MidiOutConnection* Create(s_int32 index);
         static void Destroy(MidiOutConnection* connection);
         
-        virtual bool s_bool SendNoteOn(s_uint16 note, s_uint16, velocity, s_uint16 channel) = 0;
-        virtual s_bool SendNoteOff(s_uint16 note, s_uint16, velocity, s_uint16 channel) = 0;
-        virtual s_bool SendControlChange(s_uint16 control, s_uint16, value, s_uint16 channel) = 0;
+        virtual s_bool SendNoteOn(s_uint16 note, s_uint16 velocity, s_uint16 channel) = 0;
+        virtual s_bool SendNoteOff(s_uint16 note, s_uint16 velocity, s_uint16 channel) = 0;
+        virtual s_bool SendControlChange(s_uint16 control, s_uint16 value, s_uint16 channel) = 0;
         virtual s_bool SendProgramChange(s_uint16 program, s_uint16 channel) = 0;
-        virtual _bool SendAftertouch(s_uint16 value, s_uint16 channel) = 0;
+        virtual s_bool SendAftertouch(s_uint16 value, s_uint16 channel) = 0;
         virtual s_bool SendPolyKeyPressure(s_uint16 note, s_uint16 value, s_uint16 channel) = 0;
         virtual s_bool SendModWheel(s_uint16 value, s_uint16 channel) = 0;
         
