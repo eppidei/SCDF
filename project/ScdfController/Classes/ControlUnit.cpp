ba25f2698d42de55235a5f852ba7095348e60256
athos <athos@6c43cc0a-5ffe-4778-b326-04effe5da469>
2014-12-21 19:09:33 +0000

    control unit - setters and real properties and midi connection

--- project/ScdfController/Classes/ControlUnit.cpp
+++ project/ScdfController/Classes/ControlUnit.cpp
@@ -1,53 +1,160 @@
 /*
  * ControlUnit.cpp
- *
  *  Created on: 21 Dec 2014
  *      Author: athos
  */
 
+#include "ControlUnit.h"
+#include <sstream>
+
 using namespace ScdfCtrl;
+
+ControlUnit::ControlUnit()
+{
+	midiConnection = NULL;
+	// init osc sender
+
+	oscIp = "127.0.0.1";
+	oscPort = 5555;
+	oscTag = "";
+	oscEnabled = false;
+
+	midiMsgType = Invalid;
+	midiChannel = -1;
+	midiControl = -1;
+}
 
 std::string ControlUnit::GetOscIp()
 {
-	return "sto.caz.zo!";
+	return oscIp;
 }
 
 s_int32 ControlUnit::GetOscPort()
 {
-	return 5555;
+	return oscPort;
 }
 
 std::string ControlUnit::GetOscTag()
 {
-	return "/sto/caz/zo";
+	if (!oscTag.empty()) return oscTag;
+
+	std::stringstream ss;
+	ss << "/";
+	switch (midiMsgType)
+	{
+		case NoteOn: ss<<"note[on|off]/"; break;
+		case ControlChange: ss<<"control/"; break;
+		case ProgramChange: ss<<"program/"; break;
+		case PitchBend: ss<<"pitchbend/"; break;
+		case Aftertouch: ss<<"aftertouch/"; break;
+		case PolyKeyPressure: ss<<"polypressure/"; break;
+		default: return "[invalid]";
+	}
+	if (-1==midiChannel) return "[no channel]";
+	ss << midiChannel << "/";
+	return ss.str();
 }
 
 s_bool ControlUnit::IsOscEnabled()
 {
-	return true;
+	return oscEnabled;
 }
 
 s_int32 ControlUnit::GetMidiOutIndex()
 {
-	return 0;
+	if (NULL==midiConnection) return -1;
+	return 0; //midiConnection->GetOutIndex();
 }
 
-s_int32 ControlUnit::GetMidiMessageType()
+MidiMessageType ControlUnit::GetMidiMessageType()
 {
-	return 0x80;
+	return midiMsgType;
 }
 
 s_int32 ControlUnit::GetMidiChannel()
 {
-	return 15;
+	return midiChannel;
 }
 
 s_int32 ControlUnit::GetMidiControl()
 {
-	return 62;
+	return midiControl;
 }
 
-s_int32 ControlUnit::GetMidiPitch()
+
+void ControlUnit::SetOscIp(std::string ip)
 {
-	return GetMidiControl();
+	oscIp = ip;
 }
+
+void ControlUnit::SetOscPort(s_int32 port)
+{
+	oscPort = port;
+}
+
+void ControlUnit::SetOscEnabled(s_bool enabled)
+{
+	oscEnabled = enabled;
+}
+
+s_bool ControlUnit::SetMidiOutIndex(s_int32 index)
+{
+	if (midiConnection)
+		Scdf::MidiOutConnection::Destroy(midiConnection);
+	midiConnection = Scdf::MidiOutConnection::Create(index);
+	midiConnection->SetListener(this);
+	return midiConnection != NULL;
+}
+
+void ControlUnit::SetMidiMessageType(MidiMessageType type)
+{
+	midiMsgType = type;
+}
+
+void ControlUnit::SetMidiChannel(s_int32 chan)
+{
+	midiChannel = chan;
+}
+
+void ControlUnit::SetMidiControl(s_int32 ctrl)
+{
+	midiControl = ctrl;
+}
+
+s_bool ControlUnit::SendValue(s_int32 number, s_int32 value)
+{
+	SetMidiControl(number);
+	return SendValue(value);
+}
+
+s_bool ControlUnit::SendValue(s_int32 value)
+{
+	if (oscEnabled) {
+		// send via osc
+	}
+
+	if (!midiConnection)
+		return false;
+
+	switch (midiMsgType) {
+		case NoteOn:		return midiConnection->SendNoteOn(GetMidiPitch(),value,midiChannel);
+		case NoteOff:		return midiConnection->SendNoteOff(GetMidiPitch(),value,midiChannel);
+		case ControlChange: return midiConnection->SendControlChange(GetMidiControl(),value,midiChannel);
+		case ProgramChange: return midiConnection->SendProgramChange(value,midiChannel);
+		case PitchBend: 	return midiConnection->SendModWheel(value,midiChannel);
+		case Aftertouch: 	return midiConnection->SendAftertouch(value,midiChannel);
+		case PolyKeyPressure: return midiConnection->SendPolyKeyPressure(GetMidiPitch(),value,midiChannel);
+		default: return false;
+	}
+	return false;
+
+}
+
+void ControlUnit::OnConnectionLost(Scdf::MidiOutConnection* connection)
+{
+	if (connection==midiConnection) {
+		Scdf::MidiOutConnection::Destroy(midiConnection);
+		midiConnection = NULL;
+	}
+}
+
--- project/ScdfController/Classes/ControlUnit.h
+++ project/ScdfController/Classes/ControlUnit.h
@@ -9,12 +9,19 @@
 #define CONTROLUNIT_H_
 
 #include "TypeDefinitions.h"
+#include "MidiUtils.h"
+#include "MidiOutConnection.h"
+#include <string>
 
 namespace ScdfCtrl {
 
-class ControlUnit {
+class ControlUnit : public Scdf::MidiOutConnection::Listener {
 
 public:
+
+	ControlUnit();
+
+	// Get Properties:
 
 	std::string GetOscIp();
 	s_int32 GetOscPort();
@@ -22,11 +29,48 @@ public:
 	s_bool IsOscEnabled();
 
 	s_int32 GetMidiOutIndex();
-	s_int32 GetMidiMessageType();
+	MidiMessageType GetMidiMessageType();
 	s_int32 GetMidiChannel();
 
 	s_int32 GetMidiControl();
-	s_int32 GetMidiPitch();
+	s_int32 GetMidiPitch() { return GetMidiControl(); }
+
+	// Set Properties:
+
+	void SetOscIp(std::string ip);
+	void SetOscPort(s_int32 port);
+	void SetOscEnabled(s_bool enabled);
+
+	s_bool SetMidiOutIndex(s_int32 index);
+	void SetMidiMessageType(MidiMessageType type);
+	void SetMidiChannel(s_int32 chan);
+
+	void SetMidiControl(s_int32 ctrl);
+	void SetMidiPitch(s_int32 pitch) { SetMidiControl(pitch); }
+
+	// Actions:
+
+	s_bool SendValue(s_int32 value);
+	s_bool SendValue(s_int32 number, s_int32 value);
+
+	// Listening callbacks:
+
+	 void OnConnectionLost(Scdf::MidiOutConnection* connection);
+
+private:
+
+	Scdf::MidiOutConnection* midiConnection;
+	// osc sender
+
+	std::string oscIp;
+	s_int32 oscPort;
+	std::string oscTag;
+	s_bool oscEnabled;
+
+	MidiMessageType midiMsgType;
+	s_int32 midiChannel;
+	s_int32 midiControl; // when midiMsgType is noteon/off, this is the pitch
+
 
 };
 
