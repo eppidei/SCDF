5e6c76d82ee2bd1d97955282063d90c03f4f9567
athos <athos@6c43cc0a-5ffe-4778-b326-04effe5da469>
2014-12-21 16:04:26 +0000

    midiutils

---
+++ trunk/src/include/MidiUtils.h
@@ -0,0 +1,24 @@
+/*
+ * MidiUtils.h
+ *
+ *  Created on: 21 Dec 2014
+ *      Author: athos
+ */
+
+#ifndef MIDIUTILS_H_
+#define MIDIUTILS_H_
+
+
+enum MidiMessageType {
+	Invalid = 0x0,
+	NoteOn = 0x90,
+	NoteOff = 0x80,
+	Aftertouch = 0xA0,
+	ControlChange = 0xB0,
+	ProgramChange = 0xC0,
+	PolyKeyPressure = 0xD0,
+	PitchBend = 0xE0,
+	NonMusical = 0xF0
+};
+
+#endif /* MIDIUTILS_H_ */
