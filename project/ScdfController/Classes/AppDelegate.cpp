1067b41bc2c57a62867e136b21964de1a83c73d2
athos <athos@6c43cc0a-5ffe-4778-b326-04effe5da469>
2015-01-10 15:22:12 +0000

    screen info

--- project/ScdfController/Classes/AppDelegate.cpp
+++ project/ScdfController/Classes/AppDelegate.cpp
@@ -6,6 +6,8 @@
 #include "SCDFCScrollView.h"
 #include "ControlUnit.h"
 #include "PropertiesPanel.h"
+#include <platform/CCDevice.h>
+#include "Logging.h"
 
 using namespace SCDFC;
 USING_NS_CC;
@@ -29,6 +31,32 @@ bool AppDelegate::applicationDidFinishLaunching() {
 
     // turn on display FPS
     director->setDisplayStats(true);
+
+
+    Size designSize = director->getOpenGLView()->getDesignResolutionSize();
+    LOGD("Design resolution size: %f %f",designSize.width,designSize.height);
+    Vec2 vOrigin = director->getVisibleOrigin();
+    LOGD("Visible origin: %f %f",vOrigin.x,vOrigin.y);
+    Size vSize = director->getVisibleSize();
+    LOGD("Visible size: %f %f",vSize.width,vSize.height);
+    Size frameSize = director->getOpenGLView()->getFrameSize();
+    LOGD("FRAME size: %f %f",frameSize.width,frameSize.height);
+    //int orientation = director->getOpenGLView()->getDeviceOrientation();
+    //LOGD("Orientation: %d",orientation);
+    //float zoomfactor = director->getOpenGLView()->getFrameZoomFactor();
+    //LOGD("Zoom factor: %f",zoomfactor);
+    //int retinafactor = director->getOpenGLView()->getRetinaFactor();
+    //LOGD("Retina factor: %d",retinafactor);
+    float scaleX = director->getOpenGLView()->getScaleX();
+    float scaleY = director->getOpenGLView()->getScaleY();
+    LOGD("Scale X: %f - Y: %f",scaleX,scaleY);
+    Rect vRect = director->getOpenGLView()->getVisibleRect();
+    LOGD("Visible RECT X:%f Y:%f W:%f H:%f",vRect.origin.x, vRect.origin.y, vRect.size.width,vRect.size.width);
+
+    LOGD("DPI : %d", Device::getDPI() );
+
+    director->getOpenGLView()->setDesignResolutionSize(frameSize.width,frameSize.height,ResolutionPolicy::SHOW_ALL);
+    director->setContentScaleFactor(3.0);
 
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0 / 60);
