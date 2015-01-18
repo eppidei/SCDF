2c26bee34572701d2fc5dc0e8f68100b4ebf07f7
leonardo <leonardo@6c43cc0a-5ffe-4778-b326-04effe5da469>
2015-01-18 17:56:36 +0000

    added missing files of previous commit

--- trunk/src/InitFramework.cpp
+++ trunk/src/InitFramework.cpp
@@ -11,11 +11,27 @@
 #include "PipesManager.h"
 #include "Sensor.h"
 #include "SensorsManager.h"
+#include "Receiver.h"
+
+#define USE_RECEIVER
 
 void InitFramework()
 {
+    SCDF_Receiver_T *p_receiver=NULL;
+    size_t rx_pkt_size=1024;
+    unsigned int audio_buf_len = 512;
+    unsigned int sensor_buf_len = 64;
+    unsigned int graph_buf_len = 4*audio_buf_len;
+    
     scdf::thePipesManager()->CreateReturnPipes();
     scdf::theSensorManager()->CreateAllSensors();
+#ifdef USE_RECEIVER
+    SCDF_Receiver_Init(&p_receiver, rx_pkt_size,audio_buf_len,sensor_buf_len,graph_buf_len);
+    SCDF_Receiver_SetRemoteIp(p_receiver,192,168,1,55);
+    SCDF_Receiver_SetLocalIp(p_receiver,192,168,1,54);
+    SCDF_Receiver_SetPort(p_receiver,50005);
+    SCDF_Receiver_Start(p_receiver);
+#endif
 }
 
 
