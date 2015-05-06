4bb0225dc6cb4b896a9726db3016062e86dba20a
athos <athos@6c43cc0a-5ffe-4778-b326-04effe5da469>
2015-05-06 23:05:45 +0000

    missing android srcs & svn tidy up

---
+++ trunk/project/ScdfController/Classes/android/InAppAndroid.cpp
@@ -0,0 +1,16 @@
+/*
+ * InAppAndroid.cpp
+ *
+ *  Created on: 6 May 2015
+ *      Author: athos
+ */
+
+bool CheckIsInAppPurchased(int index)
+{
+	return true;
+}
+
+bool CheckIsInAppPurchasedNoPrompt(int index)
+{
+	return true;
+}
--- trunk/project/ScdfController/proj.android/jni/Application.mk
+++ trunk/project/ScdfController/proj.android/jni/Application.mk
@@ -2,7 +2,9 @@ APP_ABI := armeabi-v7a
 APP_STL := c++_static
 NDK_TOOLCHAIN_VERSION=clang3.4
 
-APP_CPPFLAGS := -fexceptions -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char
+APP_CPPFLAGS := -fexceptions -frtti -DCC_ENABLE_CHIPMUNK_INTEGRATION=1 -std=c++11 -fsigned-char 
+APP_CFLAGS := -DADE_TARGET_TYPE=3 -DADE_BLAS_IMPLEMENTATION=1
+			#     ADE_ANDROID 			# CBLAS
 APP_LDFLAGS := -latomic -lstdc++
 APP_PLATFORM := android-14
 
