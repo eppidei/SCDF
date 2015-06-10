b8404953c3b9c3eef76f7dc414bbf9b781c925f3
marco <marco@6c43cc0a-5ffe-4778-b326-04effe5da469>
2015-06-10 23:56:36 +0000

    SCDF PLOT: draw waveform using bezier function

--- project/TestIOS/SCDF_Test/AudioWaveView.mm
+++ project/TestIOS/SCDF_Test/AudioWaveView.mm
@@ -7,17 +7,20 @@
 //
 
 #import "AudioWaveView.h"
+#import "UIBezierPath+Interpolation.h"
 
 @implementation AudioWaveView
 
-const int bufferSize = 256;
+const int bufferSize = 512;
 const int borderSize = 0;
 #define ARC4RANDOM_MAX      0x100000000
 
+
 //#define USE_TIMER_FOR_TESTING
+#define TIMER_RATE 0.05
 
 
- void AudioWaveListener::draw_buffer(s_sample *p_buff, unsigned int buff_len)
+void AudioWaveListener::draw_buffer(s_sample *p_buff, unsigned int buff_len)
 {
     if(audioWaveViewRef)
         [audioWaveViewRef OnNewBufferReady:p_buff withSize:buff_len];
@@ -46,15 +49,15 @@ const int borderSize = 0;
             bufferData[i]=(value);
             bufferDataTemp[i]=(value);
         }
-
+        
         currentBufferSize = bufferSize;
         bufferLock = [[NSObject alloc] init];
-
-    
+        
+        
 #ifdef USE_TIMER_FOR_TESTING
         bufferDataTest = (s_sample*)malloc(bufferSize*sizeof(s_sample));
         [self generateRandomBuffer];
-        [NSTimer scheduledTimerWithTimeInterval:0.002
+        [NSTimer scheduledTimerWithTimeInterval:TIMER_RATE
                                          target:self
                                        selector:@selector(onTimerTriggered:)
                                        userInfo:nil
@@ -103,7 +106,7 @@ const int borderSize = 0;
         [self BufferSizeCheck:size];
         memcpy(bufferData, buff, size*sizeof(s_sample));
     }
-   // [self setNeedsDisplay];
+    // [self setNeedsDisplay];
     [self performSelectorOnMainThread:@selector(refresh) withObject:nil waitUntilDone:NO];
 }
 
@@ -130,33 +133,115 @@ const int borderSize = 0;
 {
     // Drawing code
     
-    CGRect frame = self.frame;
     
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSetStrokeColor(context, CGColorGetComponents([[UIColor blueColor] CGColor]));
-    s_sample bufferStep = (s_sample)currentBufferSize/(s_sample)self.frame.size.width;
+    bool useBezier = true;
+    bool fillPlot = true;
     
     @synchronized (bufferLock){
         memcpy(bufferDataTemp, bufferData, currentBufferSize*sizeof(s_sample));
-     }
+    }
+    s_sample bufferStep = (s_sample)currentBufferSize/(s_sample)self.frame.size.width;
     
-   
-    for(int i = 0; i<frame.size.width - 1; i++)
+    CGRect frame = self.frame;
+    int yCenter = frame.size.height/2;
+    
+    
+    if(useBezier)
+    {
+       
+        NSMutableArray *interpolationPoints = [[NSMutableArray alloc]init];
+        
+        
+        CGPoint ptStart;
+        ptStart.x = 0;
+        ptStart.y =  borderSize + yCenter;
+        const char *encoding = @encode(CGPoint);
+        [interpolationPoints addObject:[NSValue valueWithBytes:&ptStart objCType:encoding]];
+        
+        bool somethingToDraw = false;
+        for(int i = 0; i<frame.size.width; i+=6)
         {
-            
             CGPoint pt1;
             pt1.x = i;
-            pt1.y =  borderSize + ((frame.size.height - (2*borderSize))*bufferDataTemp[(int)(i*bufferStep)]);
+            pt1.y =  borderSize + ((frame.size.height-(2*borderSize))*bufferDataTemp[(int)((i)*bufferStep)]);
+            [interpolationPoints addObject:[NSValue valueWithBytes:&pt1 objCType:encoding]];
+            
+            if(0!=pt1.y)
+                somethingToDraw = true;
+        }
+        
+        
+        CGPoint ptEnd;
+        ptEnd.x = frame.size.width;
+        ptEnd.y =  borderSize + yCenter;
+        [interpolationPoints addObject:[NSValue valueWithBytes:&ptEnd objCType:encoding]];
+        
+        
+        if (somethingToDraw) {
+            // Curve drawing
+            UIBezierPath *path = [UIBezierPath interpolateCGPointsWithHermite: interpolationPoints closed:NO];
+            
+            
+            if (path) {
+                [[UIColor blackColor] setStroke];
+                path.lineWidth = 1.5;
+                [path stroke];
+                
+                if(fillPlot)
+                {
+                    [[UIColor blackColor] setFill];
+                    path.usesEvenOddFillRule = YES;
+                    [path fill];
+                }
+            }
+
+        }
+        
+        [interpolationPoints release];
+
+    } else
+    {
+        
+        CGPoint pt1;
+        pt1.x = 0;
+        pt1.y =  borderSize + ((frame.size.height - (2*borderSize))*bufferDataTemp[0]);
+        
+        
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGMutablePathRef path = CGPathCreateMutable();
+        CGPathMoveToPoint(path, nil, pt1.x, pt1.y);
+        CGPathAddLineToPoint(path, nil, pt1.x, yCenter);
+        
+        
+        bool somethingToDraw = false;
+        for(int i = 1; i<frame.size.width; i++)
+        {
             
             CGPoint pt2;
             pt2.x = i;
-            pt2.y =  borderSize + ((frame.size.height-(2*borderSize))*bufferDataTemp[(int)((i+1)*bufferStep)]);
+            pt2.y =  borderSize + ((frame.size.height-(2*borderSize))*bufferDataTemp[(int)((i)*bufferStep)]);
+            CGPathAddLineToPoint(path, nil, pt2.x, pt2.y);
             
+            if(fillPlot)
+                CGPathAddLineToPoint(path, nil, pt2.x, yCenter);
             
-            CGContextMoveToPoint(context,pt1.x,pt1.y);
-            CGContextAddLineToPoint(context,pt2.x,pt2.y);
-            CGContextStrokePath(context);
+            if(0!=pt2.y)
+                somethingToDraw = true;
         }
+        
+        CGContextAddPath(context, path);
+        CGContextSetAllowsAntialiasing(context, NO);
+        // Do the drawing
+        
+        if(somethingToDraw)
+            CGContextDrawPath(context, kCGPathStroke);
+    }
+    
+    
+    
+
+    
+    
 }
 
 
