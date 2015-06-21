3c6b126c26f5d43d52aa53c8b2b6057fb6fe72d9
andrea <andrea@6c43cc0a-5ffe-4778-b326-04effe5da469>
2015-06-21 11:51:10 +0000

    Added SCDF and ADE as static library with visibility (iOS)

---
+++ trunk/project/ADE_Framework/ADE_Framework/ADE_Framework.m
@@ -0,0 +1,13 @@
+//
+//  ADE_Framework.m
+//  ADE_Framework
+//
+//  Created by andrea scuderi on 21/06/15.
+//  Copyright (c) 2015 fuzone. All rights reserved.
+//
+
+#import "ADE_Framework.h"
+
+@implementation ADE_Framework
+
+@end
