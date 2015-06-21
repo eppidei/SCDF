f02b2ed09fd3ca336b9453118d23b1d7acbdd49d
andrea <andrea@6c43cc0a-5ffe-4778-b326-04effe5da469>
2015-06-21 11:51:10 +0000

    Added SCDF and ADE as static library with visibility (iOS)

---
+++ project/ADE_Framework/ADE_Framework/ADE_Framework.h
@@ -0,0 +1,13 @@
+//
+//  ADE_Framework.h
+//  ADE_Framework
+//
+//  Created by andrea scuderi on 21/06/15.
+//  Copyright (c) 2015 fuzone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADE_Framework : NSObject
+
+@end
---
+++ project/SDCF_Framework_iOS/SDCF_Framework_iOS/SDCF_Framework_iOS.m
@@ -0,0 +1,13 @@
+//
+//  SDCF_Framework_iOS.m
+//  SDCF_Framework_iOS
+//
+//  Created by andrea scuderi on 20/06/15.
+//  Copyright (c) 2015 fuzone. All rights reserved.
+//
+
+#import "SDCF_Framework_iOS.h"
+
+@implementation SDCF_Framework_iOS
+
+@end
