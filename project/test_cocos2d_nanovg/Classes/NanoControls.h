d4a55615c65fb89c68223df17a630347b4353029
andrea <andrea@6c43cc0a-5ffe-4778-b326-04effe5da469>
2015-06-27 19:53:50 +0000

    NanoVG: audio input plot

--- project/test_cocos2d_nanovg/Classes/NanoControls.h
+++ project/test_cocos2d_nanovg/Classes/NanoControls.h
@@ -10,8 +10,11 @@
 #define test_cocos2d_nanovg_NanoControls_h
 
 #include "NanoControlBase.h"
+#include "ScdfSensorAPI.h"
+#include "ThreadUtils.h"
 
 struct DemoData;
+
 namespace NanoControls
 {
     class Demo : public NanoControlBase<Demo>
@@ -30,10 +33,39 @@ namespace NanoControls
 
     class Graph : public NanoControlBase<Graph>
     {
-        void OnTouchDown() override;
-        void OnTouchUp() override;
-        void OnTouchMoving() override;
+        void OnInit() override;
+        void OnTouchDown() override {}
+        void SetFillGraph(bool fill) { fillGraph=fill;}
+        void OnTouchUp() override {}
+        void OnTouchMoving() override {}
         void OnDraw() override;
+        
+        bool fillGraph;
+    };
+    
+    class AudioPlotter : public NanoControlBase<AudioPlotter>, public scdf::HarvesterListener
+    {
+    public:
+        void SetData(std::vector<float> d)
+        {
+            scdf::ThreadUtils::AutoLock kk(&dataLock);
+            data = d;
+        }
+        void SetColor(cocos2d::Color4B _c) { c=_c;}
+        void OnHarvesterBufferReady(std::vector<scdf::SensorData*> *buffer);
+        void Init(s_int32 numFrames, s_int32 rate) {data.resize(numFrames);}
+        void Release(){}
+    private:
+        void OnDraw() override;
+        void OnInit() override;
+        void OnTouchDown() override;
+        void OnTouchUp() override {}
+        void OnTouchMoving() override {}
+        
+        bool isOn;
+        std::vector<float> data;
+        scdf::ThreadUtils::CustomMutex dataLock;
+        cocos2d::Color4B c;
     };
 }
 #endif
